4f91937477f31468f6dc5ea1a0eb719a9b8912d0
EmersonAhumada <emerson.ahumada@gmail.com>
2021-09-30 09:53:51 +0000

    add+

--- src/js/index.js
+++ src/js/index.js
@@ -33,19 +33,19 @@ setInterval(function() {
 	const three = Math.floor((counter / 100) % 10);
 	const two = Math.floor((counter / 10) % 10);
 	const one = Math.floor((counter / 1) % 10);
-
-	//render your react application
-	ReactDOM.render(
-		<SimpleCounter
-			digitOne={one}
-			digitTwo={two}
-			digitThree={three}
-			digitFour={four}
-			digitFive={five}
-			digitSix={six}
-		/>,
-		document.querySelector("#app")
-	);
+	counter++,
+		//render your react application
+		ReactDOM.render(
+			<SimpleCounter
+				digitOne={one}
+				digitTwo={two}
+				digitThree={three}
+				digitFour={four}
+				digitFive={five}
+				digitSix={six}
+			/>,
+			document.querySelector("#app")
+		);
 }, 1000);
 
 SimpleCounter.propTypes = {
