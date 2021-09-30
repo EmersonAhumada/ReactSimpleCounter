4fd845960fffc2bce6d87c29d0fc06de502662a2
EmersonAhumada <emerson.ahumada@gmail.com>
2021-09-30 09:37:26 +0000

    fix num

--- src/js/index.js
+++ src/js/index.js
@@ -27,12 +27,12 @@ export function SimpleCounter(props) {
 
 let counter = 0;
 setInterval(function() {
-	const six = Math.floor(counter / 100000);
-	const five = Math.floor(counter / 10000);
-	const four = Math.floor(counter / 1000);
-	const three = Math.floor(counter / 100);
-	const two = Math.floor(counter / 10);
-	const one = Math.floor(counter / 1);
+	const six = Math.floor((counter / 100000) % 10);
+	const five = Math.floor((counter / 10000) % 10);
+	const four = Math.floor((counter / 1000) % 10);
+	const three = Math.floor((counter / 100) % 10);
+	const two = Math.floor((counter / 10) % 10);
+	const one = Math.floor((counter / 1) % 10);
 
 	//render your react application
 	ReactDOM.render(
